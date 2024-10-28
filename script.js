cd935508555b1fa68553ae43357c19f52b5c7728
Ranindi Gunasekera <ngunasekera3@academic.rrc.ca>
2024-10-28 10:51:46 -0500

    Created files, a function with a promise and an async function to fetch data from Cat Facts API

---
+++ script.js
@@ -0,0 +1,40 @@
+const condition = true;
+
+function simulatedAsync(){
+    const myPromise = new Promise((resolve, reject) => {
+        console.log("start");
+
+        setTimeout(() => {
+            if (condition) {
+                resolve("Promise is done.");
+            }
+            else{
+                reject("Promise failed.")
+            }
+        }, 3000)
+    });
+
+    myPromise.then(
+        function(result) {
+            console.log(result)
+        }
+    )
+
+    myPromise.catch(
+        function(error) {
+            console.error(error)
+        }
+    )
+}
+
+simulatedAsync();
+
+async function fetchCatBreeds() {
+    console.log("Starting async operation");
+
+    const response = await fetch("https://catfact.ninja/breeds");
+    return response.json();
+}
+
+fetchCatBreeds().then((response) => console.log(response));
+
